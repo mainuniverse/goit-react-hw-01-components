3134fd4fa83da685ece90668a145536c79575a7b
Maria <54138760+mainuniverse@users.noreply.github.com>
2021-12-20 18:37:29 +0200

    add

---
+++ src/components/Statistics/StatisticList.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import Statistics from './Statistics';
+import s from './Statistics.module.css';
+import generateColor from './generateColor';
+
+const StatisticList = ({ stats }) => {
+    return (
+        <div className={s.wrapper}>
+            <ul className={s.statList}>
+                {stats.map(stat => (
+                    <li
+                        className={s.item}
+                        key={stat.id}
+                        style={{ backgroundColor: generateColor() }}
+                    >
+                        <Statistics
+                            label={stat.label}
+                            percentage={stat.percentage}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+StatisticList.propTypes = {
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+        }),
+    ),
+};
+
+export default StatisticList;
---
+++ src/components/Statistics/generateColor.js
@@ -0,0 +1,13 @@
+const generateColor = () => {
+    const color =
+        'rgb(' +
+        Math.round(Math.random() * 255) +
+        ',' +
+        Math.round(Math.random() * 255) +
+        ',' +
+        Math.round(Math.random() * 255) +
+        ')';
+    return color;
+};
+
+export default generateColor;
